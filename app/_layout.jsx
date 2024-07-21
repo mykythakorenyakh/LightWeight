3cad43a65fa9e312a0ae6c955a1dc3b98600dcbe
Nikita Antonenko <32226532+HUKUTAXVIII@users.noreply.github.com>
2024-07-21 23:16:15 +0300

    Init commit

---
+++ app/_layout.jsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Slot } from 'expo-router'
+
+const MainLayout = () => {
+  return (
+    <>
+      <Slot />
+    </>
+  )
+}
+
+export default MainLayout
